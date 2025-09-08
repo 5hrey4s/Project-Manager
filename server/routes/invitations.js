9a2aa0b789ee86a60b7c915e3687a03a47dfb0cc
Shreyas <shreyassalyan917@gmail.com>
2025-09-08 11:18:15 +0530

    implemented notification feature for status change

--- server/routes/invitations.js
+++ server/routes/invitations.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../middleware/auth');
-const projectController = require('../controllers/invitationController');
+const invitationController = require('../controllers/invitationController');
 
 // @route   GET /api/invitations
 // @desc    Get all pending invitations for the logged-in user
 // @access  Private
-router.get('/', auth, projectController.getPendingInvitations);
+router.get('/', auth, invitationController.getPendingInvitations);
 
 // @route   POST /api/invitations/:invitationId/accept
 // @desc    Accept a project invitation
 // @access  Private (Invited user only)
-router.post('/:invitationId/accept', auth, projectController.acceptInvitation);
+router.post('/:invitationId/accept', auth, invitationController.acceptInvitation);
 
 // @route   POST /api/invitations/:invitationId/decline
 // @desc    Decline a project invitation
 // @access  Private (Invited user only)
-router.post('/:invitationId/decline', auth, projectController.declineInvitation);
+router.post('/:invitationId/decline', auth, invitationController.declineInvitation);
 
 module.exports = router;
