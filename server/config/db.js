a9d569325a82cb16b8a648a9ca4bccca1b152402
Shreyas <shreyassalyan917@gmail.com>
2025-09-15 19:30:59 +0530

    integrated github

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,12 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+// The Supabase host from your connection string
+const supabaseHost = 'db.xvpoazghxemrpiwqebgl.supabase.co'; // IMPORTANT: Use your OLD project host
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    host: supabaseHost, // This forces the connection over IPv4
     ssl: {
         rejectUnauthorized: false
     }
